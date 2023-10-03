39439776f1d0def98b659c12c4e65c078a272454
Moussa Niang <niangm@ept.sn>
2023-10-03 15:28:13 +0000

    .

--- microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
+++ microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
@@ -2,15 +2,12 @@ package com.mproduits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-@EnableDiscoveryClient
 public class MproduitsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MproduitsApplication.class, args);
 	}
