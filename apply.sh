09eff0fce198960490c48c786c749ed153e42810
Moussa Niang <niangm@ept.sn>
2023-10-06 12:23:42 +0000

    .

--- apply.sh
+++ apply.sh
@@ -7,8 +7,8 @@ kubectl apply -f config-server-service.yaml
 kubectl apply -f mproduits-deployment.yaml
 kubectl apply -f mproduits-service.yaml
 
-kubectl apply -f mpaiement-deployment.yaml
-kubectl apply -f mpaiement-service.yaml
+# kubectl apply -f mpaiement-deployment.yaml
+# kubectl apply -f mpaiement-service.yaml
 
-kubectl apply -f mcommandes-deployment.yaml
-kubectl apply -f mcommandes-service.yaml
+# kubectl apply -f mcommandes-deployment.yaml
+# kubectl apply -f mcommandes-service.yaml
