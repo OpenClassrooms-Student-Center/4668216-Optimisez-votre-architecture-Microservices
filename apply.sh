e3a2cd5a9c4d5cf3e51bde41c1af5a2b1ece6db4
Moussa Niang <niangm@ept.sn>
2023-10-09 12:30:11 +0000

    Introduce sleep time between service creations on k8s

--- apply.sh
+++ apply.sh
@@ -4,8 +4,14 @@ cd Kubernetes
 kubectl apply -f config-server-deployment.yaml
 kubectl apply -f config-server-service.yaml
 
+sleep(180)
+
 kubectl apply -f mproduits-deployment.yaml
 kubectl apply -f mproduits-service.yaml
+
+sleep(180)
+
+kubectl apply -f ingress-controller.yaml
 
 # kubectl apply -f mpaiement-deployment.yaml
 # kubectl apply -f mpaiement-service.yaml
